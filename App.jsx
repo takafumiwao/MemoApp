fd09eaea5ef5049128afe62c38bd79d573639b12
iwaotakashi <37037413+takafumiwao@users.noreply.github.com>
2021-12-23 21:38:53 +0900

    Make components touchable

--- App.jsx
+++ App.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import MemoListScreen from "./src/screens/MemoListScreen";
 
 // import LogInScreen from "./src/screens/LogInScreen";
-import SignUpScreen from "./src/screens/SignUpScreen";
+// import SignUpScreen from "./src/screens/SignUpScreen";
 
 
 export default function App() {
 	return (
-		<SignUpScreen />
+		<MemoListScreen />
 	);
 }
