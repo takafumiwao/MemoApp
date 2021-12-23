cc19cb76dc0d72e9c5bd1e227999513c5df83a1a
iwaotakashi <37037413+takafumiwao@users.noreply.github.com>
2021-12-23 21:24:26 +0900

    Create LogInScreen and SignUpScreen

--- App.jsx
+++ App.jsx
@@ -1,13 +1,11 @@
 import React from "react";
-import MemoCreateScreen from "./src/screens/MemoCreateScreen";
 
-// import MemoDetailScreen from "./src/screens/MemoDetailScreen";
-// import MemoEditScreen from "./src/screens/MemoEditScreen";
-// import MemoListScreen from "./src/screens/MemoListScreen";
+// import LogInScreen from "./src/screens/LogInScreen";
+import SignUpScreen from "./src/screens/SignUpScreen";
 
 
 export default function App() {
 	return (
-		<MemoCreateScreen />
+		<SignUpScreen />
 	);
 }
