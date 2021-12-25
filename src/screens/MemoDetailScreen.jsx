ee8faeddc98ed9e65a467d2c369f9df5086aaaf0
iwaotakashi <37037413+takafumiwao@users.noreply.github.com>
2021-12-25 13:27:20 +0900

    Fetch data from firestore on memodetailscreen

--- src/screens/MemoDetailScreen.jsx
+++ src/screens/MemoDetailScreen.jsx
@@ -1,21 +1,42 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import { View, ScrollView, Text, StyleSheet } from "react-native";
+import { shape, string } from "prop-types";
+import firebase from "firebase";
 
 import CircleButton from "../components/CircleButton";
+import { dateToString } from "../utils";
 
 export default function MemoDetailScreen(props) {
-	const { navigation } = props;
+	const { navigation, route } = props;
+	const { id } = route.params;
+	const [memo, setMemo] = useState(null);
+	let unsubscribe = () => {};
+	useEffect(() => {
+		const { currentUser } = firebase.auth();
+		if (currentUser) {
+			const db = firebase.firestore();
+			const ref = db.collection(`users/${currentUser.uid}/memos`).doc(id);
+			unsubscribe = ref.onSnapshot((doc) => {
+				console.log("doc", doc.data());
+				const data = doc.data();
+				setMemo({
+					id: data.id,
+					bodyText: data.bodyText,
+					updatedAt: data.updatedAt.toDate(),
+				});
+			});
+		}
+		return unsubscribe;
+	}, []);
 	return (
 		<View style={styles.container}>
 			<View style={styles.memoHeader}>
-				<Text style={styles.memoTitle}>買い物リスト</Text>
-				<Text style={styles.memoDate}>2020年12月24日</Text>
+				<Text style={styles.memoTitle} numberOfLines={1}>{memo && memo.bodyText}</Text>
+				<Text style={styles.memoDate}>{memo && dateToString(memo.updatedAt)}</Text>
 			</View>
 			<ScrollView style={styles.memoBody}>
 				<Text style={styles.memoText}>
-                買い物リスト
-                てすとsててええっててうぇw
-                てwたてwtwてwてtれwtwrwとぁt要注意。
+					{memo && memo.bodyText}
 				</Text>
 			</ScrollView>
 			<CircleButton
@@ -26,6 +47,12 @@ export default function MemoDetailScreen(props) {
 		</View>
 	);
 }
+
+MemoDetailScreen.propTypes = {
+	route: shape({
+		params: shape({ id: string}),
+	}).isRequired,
+};
 
 const styles = StyleSheet.create({
 	container: {
