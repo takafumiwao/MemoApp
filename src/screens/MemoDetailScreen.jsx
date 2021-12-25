54b2959d3141c305a5eca27c31faea23b6cee5fe
iwaotakashi <37037413+takafumiwao@users.noreply.github.com>
2021-12-25 14:25:35 +0900

    Update memo data on EditScreen

--- src/screens/MemoDetailScreen.jsx
+++ src/screens/MemoDetailScreen.jsx
@@ -42,7 +42,7 @@ export default function MemoDetailScreen(props) {
 			<CircleButton
 				style={{ top: 60, bottom: "auto" }}
 				name="pencil"
-				onPress={() => { navigation.navigate("MemoEdit"); }}
+				onPress={() => { navigation.navigate("MemoEdit", { id: id, bodyText: memo.bodyText}); }}
 			/>
 		</View>
 	);
